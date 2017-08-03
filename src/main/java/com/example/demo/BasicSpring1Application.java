2a6ad110b979132181261031a978f4038327fb93
Srikant Kalaputapu <skalaputapu@gmail.com>
2017-08-03 13:35:14 -0400

    first commit

---
+++ src/main/java/com/example/demo/BasicSpring1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpring1Application.class, args);
+	}
+}
