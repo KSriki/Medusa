93afb5ca7c326de8c11a0fcd6151c3825aedb005
Srikant Kalaputapu <skalaputapu@gmail.com>
2017-08-04 11:51:06 -0400

    added simple save functions

--- src/main/java/medusa/services/CollegeService.java
+++ src/main/java/medusa/services/CollegeService.java
@@ -3,6 +3,7 @@ package medusa.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import medusa.model.College;
 import medusa.repositories.CollegeRepository;
 
 @Service
@@ -11,4 +12,9 @@ public class CollegeService {
 	@Autowired
 	CollegeRepository collegeRepository;
 
+	
+	public void saveCollege(College c) {
+		collegeRepository.save(c);
+	}
+		
 }
