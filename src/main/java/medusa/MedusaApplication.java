ff0a0e30b9e67a5f46aa64fd2a8940ec1db0d57d
Srikant Kalaputapu <skalaputapu@gmail.com>
2017-08-04 10:01:43 -0400

    Fixed names

---
+++ src/main/java/medusa/MedusaApplication.java
@@ -0,0 +1,12 @@
+package medusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedusaApplication.class, args);
+	}
+}
