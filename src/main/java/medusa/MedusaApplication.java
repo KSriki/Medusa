9b77025628d690058ff32a6652db1bcf3bdb311f
Taimur Sohrab <tsohrab8@gmail.com>
2017-08-04 09:33:31 -0400

    renamed sriki's mistake

--- src/main/java/medusa/MedusaApplication.java
+++ src/main/java/medusa/MedusaApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package medusa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicSpring1Application {
+public class MedusaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicSpring1Application.class, args);
+		SpringApplication.run(MedusaApplication.class, args);
 	}
 }
