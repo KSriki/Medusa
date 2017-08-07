a33016c9ff6084631fc33b8e6bfb2fb458ba5700
jinpeng wu <wu274319426@gmail.com>
2017-08-07 10:56:13 -0400

    add login page

---
+++ src/main/java/com/example/demo/LoginRegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginRegistrationController {
+	 
+	@RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}  
+  //  @RequestMapping("/userprofile")
+   // public String userprofile(Principal principal,Model model){
+    //	User user = userService.findByUsername(principal.getName());
+    	//System.out.println(user.getUsername());
+    //	model.addAttribute("user", user);
+      //  return "userprofile";
+//}
