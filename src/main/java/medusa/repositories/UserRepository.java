750205c216f1acddc2c18264f4acd97e75e77484
Toma Tadros <limbo43@gmail.com>
2017-08-07 10:09:17 -0400

    added basic security from samazon

--- src/main/java/medusa/repositories/UserRepository.java
+++ src/main/java/medusa/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import medusa.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	public User findByUsername(String username);
 }
