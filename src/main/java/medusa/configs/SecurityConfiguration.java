3a14b5e34c07859293da3cfad57db4a1909ddf3e
Lubaba <lubaba070@gmail.com>
2017-08-09 14:08:02 -0400

    password encoder removed

--- src/main/java/medusa/configs/SecurityConfiguration.java
+++ src/main/java/medusa/configs/SecurityConfiguration.java
@@ -9,8 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import medusa.repositories.UserRepository;
@@ -44,12 +44,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userDetailsServiceBean())
-                .passwordEncoder(encoder());
+                .userDetailsService(userDetailsServiceBean());
+  //              .passwordEncoder(encoder());
     }
-    
+    /*
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(11);
     }
+    */
 }
